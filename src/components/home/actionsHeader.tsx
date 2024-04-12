95691d0b9f36f53df7736681b2cbecade2069b54
Leandro Macedo <leandro.smacedo@Live.com>
2024-04-12 14:07:28 +0100

    feat: export clients to .JSON File
    Implemented logic to export customer list data from localStorage to a JSON file.
    
    **Functionality:**
    
    - Check if there is an array of clients (“clients”) in localStorage.
    - If it exists, convert the array to a JavaScript object using JSON.parse.
    - Transforms the array into a JSON file and a Blob object using the `downloadContacts` function.
    - Create a link to download the JSON file.
    
    **Comments:**
    - The `getContacts` and `downloadContacts` functions have been modified to meet the new export logic.
    
    **Benefits:**
    
    - Makes it easy to share and backup customer list data.
    - Allows viewing and editing of data in a text editor.

--- src/components/home/actionsHeader.tsx
+++ src/components/home/actionsHeader.tsx
@@ -1,40 +1,43 @@
-import {
-    File,
-    FileDown,
-    FileDownIcon,
-    FileUp,
-    ListFilter,
-    PlusCircle,
-} from "lucide-react"
-
-
+import { FileDown, FileUp, } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
-
-import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { NewContactDiaolog } from "./contacts/newContactDialog"
 
-type Checked = DropdownMenuCheckboxItemProps["checked"]
 
 export function ActionsHeader() {
-    const [showStatusBar, setShowStatusBar] = useState<Checked>(true)
-    const [showActivityBar, setShowActivityBar] = useState<Checked>(false)
-    const [showPanel, setShowPanel] = useState<Checked>(false)
+    const [contacts, setContacts] = useState<any>([]);
+
+    const getContacts = () => {
+        const contactsToString = localStorage.getItem('contacts');
+        if (contactsToString) {
+            const parsedContacts = JSON.parse(contactsToString) as any;
+            setContacts(parsedContacts);
+        }
+    };
+
+    const downloadContacts = () => {
+        const contactsString = JSON.stringify(contacts);
+        const blob = new Blob([contactsString], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'contacts.json';
+        link.click();
+        URL.revokeObjectURL(url);
+    };
+
+    useEffect(() => {
+        getContacts();
+    }, []);
     return (
         <>
             <div className="ml-auto flex items-center gap-2">
                 <Button size="sm" variant="outline" className="h-7 gap-1">
                     <FileDown className="h-3.5 w-3.5" />
-                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    <span
+                        onClick={downloadContacts}
+                        className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                         Export all contacts
                     </span>
                 </Button>
